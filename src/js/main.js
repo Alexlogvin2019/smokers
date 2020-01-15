6ccc83fc94e8cab8f275f950d0e9b74c2d861b88
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-01-15 15:31:58 +0200

    ver6

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,35 @@ $(document).ready(function(){
         dots: true,
       }
     );
+
+  $('#form').submit(function(){
+		var form = $(this);
+		var error = false;
+		form.find('textaria, input').each( function(){
+			if ($(this).val() == '') {
+				error = true;
+			}
+		});
+		if (!error) {
+			var data = form.serialize();
+			$.ajax({
+				type: 'POST',
+				url: './email.php',
+				dataType: 'json',
+				data: data,
+				beforeSend: function(data) {
+					form.find('.contact__button').attr('disabled', 'disabled');
+				},
+				success: function(data){
+            $('.contact__button').fadeOut('slow');
+            $('.fadein').fadeIn('slow');
+					
+				},
+				complete: function(data) {
+					form.find('.contact__button').prop('disabled', false);
+        }
+      });
+		}
+    return false;
   });
+});
